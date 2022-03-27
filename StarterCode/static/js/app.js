e8981242abfa79e26e6a47cd13e685680cd67ef7
RubyRaine <MsRubyNguyen@gmail.com>
2022-03-26 19:25:18 -0700

    Pushing up current working info

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,8 @@
+//Read in the Data
+d3.json("../Startercode/samples.json", function(data) {
+    console.log(data[0]);
+});
+
+// var sample_values = object.values();
+// var otu_ids = object.values();
+// var otu_labels = object.values();
